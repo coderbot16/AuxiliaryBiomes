cef22aa947a20686621c055c4bff6686ff9c0b60
coderbot <coderbot16@gmail.com>
2019-02-15 17:54:09 -0800

    Initial commit

---
+++ src/main/java/net/coderbot/auxbiomes/AuxiliaryBiomes.java
@@ -0,0 +1,11 @@
+package net.coderbot.auxbiomes;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = AuxiliaryBiomes.MODID, name = AuxiliaryBiomes.NAME, version = AuxiliaryBiomes.VERSION)
+public class AuxiliaryBiomes
+{
+    public static final String MODID = "auxbiomes";
+    public static final String NAME = "AuxiliaryBiomes";
+    public static final String VERSION = "1.0.0";
+}
