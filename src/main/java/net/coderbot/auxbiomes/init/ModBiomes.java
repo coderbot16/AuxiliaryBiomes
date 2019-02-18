07f2dda5f1a5b70d68da11b76151e782f24c593c
coderbot <coderbot16@gmail.com>
2019-02-17 17:38:26 -0800

    Add White Forest and Forested Island

--- src/main/java/net/coderbot/auxbiomes/init/ModBiomes.java
+++ src/main/java/net/coderbot/auxbiomes/init/ModBiomes.java
@@ -6,6 +6,7 @@ import net.coderbot.auxbiomes.biomes.BiomeWasteland;
 import net.minecraft.init.Biomes;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeForest;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.event.RegistryEvent;
@@ -17,6 +18,8 @@ public class ModBiomes {
 	public static Biome MARSH;
 	public static Biome WASTELAND;
 	public static Biome ICE_WASTELAND;
+	public static Biome FORESTED_ISLAND;
+	public static Biome WHITE_FOREST;
 
 	@SubscribeEvent
 	public static void registerBiomes(RegistryEvent.Register<Biome> event) {
@@ -54,6 +57,29 @@ public class ModBiomes {
 
 			ICE_WASTELAND = registerBiome(event, new BiomeWasteland(iceWastelandProperties, Blocks.SNOW.getDefaultState(), false), BiomeManager.BiomeType.ICY, "ice_wasteland", 5);
 			BiomeDictionary.addTypes(ICE_WASTELAND, BiomeDictionary.Type.WASTELAND, BiomeDictionary.Type.SNOWY, BiomeDictionary.Type.COLD);
+		}
+
+		{
+			Biome.BiomeProperties forestedIslandProperties = new Biome.BiomeProperties("Forested Island")
+					.setTemperature(Biomes.FOREST.getDefaultTemperature() + 0.1F)
+					.setRainfall(Biomes.FOREST.getRainfall())
+					.setBaseHeight(-1.0F)
+					.setHeightVariation(0.8F);
+
+			FORESTED_ISLAND = registerBiome(event, new BiomeForest(BiomeForest.Type.NORMAL, forestedIslandProperties), BiomeManager.BiomeType.WARM, "forested_island", 10);
+			BiomeDictionary.addTypes(FORESTED_ISLAND, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.HILLS, BiomeDictionary.Type.OCEAN, BiomeDictionary.Type.WATER);
+		}
+
+		{
+			Biome.BiomeProperties whiteForestProperties = new Biome.BiomeProperties("White Forest")
+					.setTemperature(Biomes.COLD_TAIGA_HILLS.getDefaultTemperature())
+					.setRainfall(Biomes.COLD_TAIGA_HILLS.getRainfall())
+					.setBaseHeight(0.3F)
+					.setHeightVariation(0.2F)
+					.setSnowEnabled();
+
+			WHITE_FOREST = registerBiome(event, new BiomeForest(BiomeForest.Type.NORMAL, whiteForestProperties), BiomeManager.BiomeType.ICY, "white_forest", 10);
+			BiomeDictionary.addTypes(WHITE_FOREST, BiomeDictionary.Type.COLD, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.SNOWY, BiomeDictionary.Type.HILLS);
 		}
 	}
 
